2a900f90c47e3a0b9384073e6d8c9babc9a76d52
Eduardo Bouças <mail@eduardoboucas.com>
2017-04-08 23:19:00 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,67 @@
+const path = require('path')
+
+function createTreeNode(dependencyMap, node) {
+  const children = dependencyMap[node].map(child => {
+    return createTreeNode(dependencyMap, child)
+  })
+
+  return {
+    [node]: children
+  }
+}
+
+function ComponentTreePlugin (options) {
+  this.directories = options.directories || []
+  this.extensions = options.extensions || ['.jsx']
+  this.outputPath = options.outputPath
+}
+
+ComponentTreePlugin.prototype.apply = function (compiler) {
+  compiler.plugin('this-compilation', compilation => {
+    compilation.plugin('additional-assets', callback => {
+      compilation.chunks.forEach(chunk => {
+        let dependencyMap = {}
+
+        chunk.modules.forEach(module => {
+          if (!this.isComponent(module.resource)) return
+
+          const dependencies = module.dependencies.filter(dependency => {
+            return dependency.module &&
+              dependency.module.resource &&
+              this.isComponent(dependency.module.resource)
+          }).map(dependency => dependency.module.resource)
+
+          dependencyMap[module.resource] = dependencies
+        })
+
+        const rootComponent = Object.keys(dependencyMap).find(component => {
+          return Object.keys(dependencyMap).every(innerComponent => {
+            return !dependencyMap[innerComponent].includes(component)
+          })
+        })
+
+        const dependencyTree = createTreeNode(dependencyMap, rootComponent)
+        const payload = JSON.stringify(dependencyTree)
+
+        compilation.assets[this.outputPath] = {
+          source: () => payload,
+          size: () => payload.length
+        }
+      })
+
+      callback()
+    })
+  })
+}
+
+ComponentTreePlugin.prototype.isComponent = function (modulePath) {
+  const directoryMatch = this.directories.some(directory => {
+    return modulePath.indexOf(directory) === 0
+  })
+
+  if (!directoryMatch) return false
+
+  return this.extensions.includes(path.extname(modulePath))  
+}
+
+module.exports = ComponentTreePlugin
